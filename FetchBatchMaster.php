69eb650b7fd776f4ce1c4cc530fe3ab5276860e8
HariprasathC <hariprasath.c@hcl.com>
2020-05-12 11:54:07 +0530

    hari partialy fetch

---
+++ FetchBatchMaster.php
@@ -0,0 +1,42 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json;");
+$json = file_get_contents('php://input');
+$data = json_decode($json,true);
+
+
+require_once 'include/dbconnect.php';
+
+ if(isset($data['branchname'])){
+	$branch_name = $data['branchname'];
+ }else{
+	 $branch_name = '';
+ }
+ 
+ if($branch_name){
+	 
+	 $condition ="and type = '$branch_name'";
+	 
+ }else{
+	 $condition = "";
+ }
+
+    $query = "SELECT * FROM batch_master  where is_active = '1' $condition";
+	$result = mysqli_query($conn,$query);
+	$branch_details = [];
+    
+    if(mysqli_num_rows($result) > 0){
+        while ($branchmasterrows = mysqli_fetch_assoc($result)){
+                $branch_details[] = $branchmasterrows;
+        } 
+        $errcode = 200;
+        $status = "Success";
+    }else{
+        $errcode = 400;
+        $status = "Failure";
+    }
+
+    echo $result = json_encode(array("errCode"=>$errcode,"status"=>$status,"arrRes" => $branch_details));
+
+mysqli_close($conn);
+?>
---
+++ FetchTrainingType.php
@@ -0,0 +1,42 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json;");
+$json = file_get_contents('php://input');
+$data = json_decode($json,true);
+
+
+require_once 'include/dbconnect.php';
+
+ if(isset($data['type'])){
+	$training_type = $data['type'];
+ }else{
+	 $training_type = '';
+ }
+ 
+ if($training_type){
+	 
+	 $condition ="and type = '$training_type'";
+	 
+ }else{
+	 $condition = "";
+ }
+
+	$query = "SELECT * FROM training_type where is_active = '1' $condition";
+	$result = mysqli_query($conn,$query);
+	$training_types = [];
+    
+    if(mysqli_num_rows($result) > 0){
+        while ($trainingtyperows = mysqli_fetch_assoc($result)){
+                $training_types[] = $trainingtyperows;
+        } 
+        $errcode = 200;
+        $status = "Success";
+    }else{
+        $errcode = 400;
+        $status = "Failure";
+    }
+
+    echo $result = json_encode(array("errCode"=>$errcode,"status"=>$status,"arrRes" => $training_types));
+
+mysqli_close($conn);
+?>
