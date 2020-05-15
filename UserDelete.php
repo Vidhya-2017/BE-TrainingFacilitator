f0c902083aa6447b3b21b5c2711894a69dd2061a
Arunkumar N <arunkumar-na@hcl.com>
2020-05-15 09:54:36 +0530

    Aruns Commit 15/5/2010 10am

---
+++ UserDelete.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token");
+
+$json = file_get_contents('php://input');
+$data = json_decode($json,true);
+//echo "here";
+require_once 'include/dbconnect.php';
+$id = $data['id'];
+$is_active                  = '1';
+$updated_date 			    = date('Y-m-d h:i:s');
+$updated_by				    = $data['updated_by'];
+  
+$query = "UPDATE `user_registration` SET is_active='".$is_active."', updated_by='".$updated_by."', updated_date='".$updated_date."' WHERE id='".$id."'";        
+$result = mysqli_query($conn,$query);
+if($result){
+    $errcode = 200;
+    $status = "Success";       
+}else{
+    $errcode = 404;
+    $status = "Failure";
+    $locId = "";
+}
+echo $result = json_encode(array("errCode"=>$errcode,"status"=>$status));
+
+mysqli_close($conn);
+?>
